b2764d7a51fb9f5a807208843d6c9cf765bb418a
kionlife <kionlife@gmail.com>
2021-02-09 18:56:47 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,68 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class Loading extends React.Component {
+  render() {
+    return <div className="lds-dual-ring"></div>
+  }
+}
+
+class Currencies extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      currencyList: [],
+      history: []
+    }
+    this.fetchCurrency = this.fetchCurrency.bind(this);
+
+    
+  }
+
+  fetchCurrency() {
+    this.setState({
+      loading: true
+    })
+    fetch('https://api.ifcityevent.com/currency')
+      .then(resp => resp.json())
+      .then(resp => {
+        this.setState({
+          currencyList: resp,
+          loading: false,
+          history: [...this.state.history, <p>{resp.rateBuy}</p>]
+        });
+      });
+  }
+
+  componentDidMount() {
+    this.fetchCurrency();
+
+    setInterval(
+      () => this.fetchCurrency(),
+      30000
+    );
+  }
+
+
+  render() {
+    return (
+      <div>
+        {this.state.loading ? <Loading /> : null}
+        <p>{this.state.currencyList.name} - {this.state.currencyList.rateBuy}</p>
+        <p>History</p>
+        <div>{this.state.history}</div>
+        <button onClick={this.fetchCurrency}>Оновити</button>
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+    <Currencies />
+  </div>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+{}
